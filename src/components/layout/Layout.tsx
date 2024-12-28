74589630e484b60f83253063cfa3212a12c7c24d
JAILBREAK-101 <samuelifeoluwa540@gmail.com>
2024-12-28 09:09:49 +0100

    chore: add analytics, speed insights and update next dependencies

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,12 +2,16 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 import styles from '../../styles/layout.module/Layout.module.css';
 import { TypeComponent } from '@/types/global.types';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const Layout: React.FC<TypeComponent> = ({ children }) => {
   return (
     <div className={`main-page ${styles.layout}`}>
       {/* <Navbar /> */}
       <main>{children}</main>
+      <Analytics />
+      <SpeedInsights />
       <Footer />
     </div>
   );
