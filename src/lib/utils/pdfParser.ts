120a1b6328334badcc957cd72cdf3b75815cdd50
JAILBREAK-101 <samuelifeoluwa540@gmail.com>
2024-12-28 08:34:39 +0100

    chore(pdfParser): remove unnecessary pdfparser

--- src/lib/utils/pdfParser.ts
+++ src/lib/utils/pdfParser.ts
@@ -1,17 +1,17 @@
-import * as pdfjsLib from "pdfjs-dist/webpack";
+// import * as pdfjsLib from "pdfjs-dist/webpack";
 
-export async function parsePdf(filePath) {
-  const loadingTask = pdfjsLib.getDocument(filePath);
+// export async function parsePdf(filePath) {
+//   const loadingTask = pdfjsLib.getDocument(filePath);
 
-  const pdf = await loadingTask.promise;
-  const textContent = [];
+//   const pdf = await loadingTask.promise;
+//   const textContent = [];
 
-  for (let i = 1; i <= pdf.numPages; i++) {
-    const page = await pdf.getPage(i);
-    const content = await page.getTextContent();
-    const pageText = content.items.map((item) => item.str).join(" ");
-    textContent.push(pageText);
-  }
+//   for (let i = 1; i <= pdf.numPages; i++) {
+//     const page = await pdf.getPage(i);
+//     const content = await page.getTextContent();
+//     const pageText = content.items.map((item) => item.str).join(" ");
+//     textContent.push(pageText);
+//   }
 
-  return textContent.join("\n"); // Returns combined text of all pages.
-}
+//   return textContent.join("\n"); // Returns combined text of all pages.
+// }
